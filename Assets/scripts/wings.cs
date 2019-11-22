5b6427a5ff33d82d04e5ccf94f35a75250822593
jude <Weepin0angle@gmail.com>
2019-11-21 17:18:04 -0800

    Added Comment

--- Assets/scripts/wings.cs
+++ Assets/scripts/wings.cs
@@ -7,7 +7,7 @@ public class wings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
